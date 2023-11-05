e1e8841cbef84b82bd0412538ec8e24e903016ee
Danny <111186843+dineshahire@users.noreply.github.com>
2023-11-05 06:28:30 +0530

    Add files via upload

---
+++ MultithreadingExample.java
@@ -0,0 +1,28 @@
+public class MultithreadingExample {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new MyRunnable("Thread 1"));
+        Thread thread2 = new Thread(new MyRunnable("Thread 2"));
+        thread1.start();
+        thread2.start();
+    }
+}
+
+class MyRunnable implements Runnable {
+    private String threadName;
+
+    public MyRunnable(String name) {
+        this.threadName = name;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(threadName + ": Count " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
